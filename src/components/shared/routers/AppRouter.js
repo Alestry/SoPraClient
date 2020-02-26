0009f262031abf635a7e1438f94b4903cc068b37
Alestry <45560364+Alestry@users.noreply.github.com>
2020-02-26 21:46:42 +0100

    Update AppRouter.js

--- src/components/shared/routers/AppRouter.js
+++ src/components/shared/routers/AppRouter.js
@@ -16,6 +16,7 @@ import UserID from "../../UserID/UserID";
  * /game renders a Router that contains other sub-routes that render in turn other react components
  * Documentation about routing in React: https://reacttraining.com/react-router/web/guides/quick-start
  */
+
 class AppRouter extends React.Component {
   render() {
     return (
@@ -40,7 +41,7 @@ class AppRouter extends React.Component {
               )}
             />
             <Route
-                path="/userid"
+                path='/userid/:id'
                 exact
                 render={() => (
                     <UserIDGuard>
