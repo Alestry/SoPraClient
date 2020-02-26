93509dcd6a028e9cdae9ab774dab2c5bd376a1b8
Alestry <45560364+Alestry@users.noreply.github.com>
2020-02-26 21:43:03 +0100

    Update UserID.js

--- src/components/UserID/UserID.js
+++ src/components/UserID/UserID.js
@@ -6,11 +6,64 @@ import User from '../shared/models/User';
 import { withRouter } from 'react-router-dom';
 import { Button } from '../../views/design/Button';
 
+const FormContainer = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
 class UserID extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: window.location.pathname,
+            user: null
+        };
+
+        //Not the most ideal way of handling the page's individualization, but it works i guess
+        let tempid = "";
+        let i;
+        let happend = 0;
+        for(i=0;i<this.state.id.length;i++){
+            if(happend === 2){
+                tempid += this.state.id[i];
+            }
+            if(this.state.id[i] === "/"){
+                happend += 1;
+            }
+        }
+        this.state.id = tempid;
+
+        /*const gotUser = api.get('/userid/:id', this.state.id);
+        const thisUser = new User(gotUser.data);
+        this.state.user = thisUser;*/
+    }
+
+    async goBack(){
+        this.props.history.push("/game");
+    }
 
     render() {
         return (
-            <h2>Hello</h2>
+            <FormContainer>
+                <h1>Welcome to Profile of User Nr. {this.state.id}!</h1>
+                <ButtonContainer>
+                    <Button onClick={() => {this.goBack()}}>
+                        Return
+                    </Button>
+                </ButtonContainer>
+                {this.state.id}
+            </FormContainer>
         )
     }
 }
